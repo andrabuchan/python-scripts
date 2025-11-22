b0f608bb72a7cc215643e5d5b3f1bb440bd8205f
Andra Buchan <91843821+andrabuchan@users.noreply.github.com>
2025-11-21 19:26:46 -0500

    Update filter_metacerberus_gff.py

--- filter_metacerberus_gff.py
+++ filter_metacerberus_gff.py
@@ -12,9 +12,9 @@ gff = open(args.input, 'r')
 linelist = gff.readlines()
 header = linelist[0]
 print(f"Parsing {len(linelist)} sequences...")
+
 #filters headers out of list
 def preprocess(linelist):
-
         num_removed_lines = 0
         for line in linelist:
                 #removes headers
@@ -24,7 +24,6 @@ def preprocess(linelist):
                         continue
         data = line.split('\t')
 
-        #removes those yucky fasta lines
         if len(data) < 2:
                 linelist.remove(line)
                 num_removed_lines += 1
@@ -34,20 +33,7 @@ def preprocess(linelist):
 
 #filters out hypothetical genes
 def remove_hypothetical(linelist):
-        num_hypothetical = 0
-
         linelist = [ line for line in linelist if "Hypothetical" not in line ]
-        #for line in linelist:
-        #       data = line.split('\t')
-        #       attr = data[-1].split(';')
-        #
-        #       if len(attr) < 2:
-        #               print(f"ERROR: attribute does not have at least 2 columns. ({attr})")
-        #               continue
-        #       genename = attr[1]
-        #       if "Hypothetical" in genename or "hypothetical" in genename:
-        #               linelist.remove(line)
-        #               num_hypothetical += 1
         print(f"FILTERING: removed hypothetical genes. Left with {len(linelist)} genes")
         return linelist
 
@@ -58,21 +44,20 @@ def format_names(linelist):
         for line in linelist:
                 data = line.split('\t')
                 attr = data[-1].split(';')
-                genename = attr[1]
-                if ';' in genename:
-                        print(f"gene name - {genename} contains semicolon. Fixing...")
-                        new_genename = genename.strip(';')
+                if len(attr) > 8:
+                        genename = attr[1] + ";" + attr[2]
+                        new_genename = attr[1] + attr[2]
                         index = linelist.index(line)
                         fixed_line = line.replace(genename, new_genename)
                         linelist[index] = fixed_line
                         num_semicolons += 1
         print(f"FORMATTING: removed semicolons from {num_semicolons} gene names")
-
+        return linelist
 
 
 linelist = preprocess(linelist)
 linelist = remove_hypothetical(linelist)
-#linelist = format_names(linelist)
+linelist = format_names(linelist)
 
 with open(args.output, 'w') as outfile:
         outfile.write(header)
